d4568dcc3c221b25a9227af1ae022c4f0a5b3476
Jiangping Li <jiangping.li@outlook.com>
2018-04-19 13:45:31 +0800

    Change settings

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from time import time
 
 import xlart
 
-
 if __name__ == '__main__':
     start = time()
-    xlart.resize_image('picture.jpg', 'picture_resized.jpg', 256, 384)
+    xlart.resize_image('picture.jpg', 'picture_resized.jpg', 200, 200)
     xlart.image_to_xlsx('picture_resized.jpg', 'demo.xlsx')
     end = time()
     print('An xlart file has been generated in ' + str(end - start) + 's.')
--- xlart.py
+++ xlart.py
@@ -6,7 +6,8 @@ from PIL import Image
 
 def resize_image(filename, new_filename, width, height):
     img = Image.open(filename)
-    new_img = img.resize((width, height))
+    new_img = img.convert('RGB')
+    new_img = new_img.resize((width, height))
     new_img.save(new_filename, 'JPEG', optimize=True)
 
 
@@ -18,12 +19,15 @@ def image_to_xlsx(image_file, xlsx_file):
 
     for i in range(img.width):
         for j in range(img.height):
-            ws.cell(row=j+1, column=i+1).fill = PatternFill(fgColor=Color(
-                '{0:02x}{1:02x}{2:02x}'.format(pixel[i, j][0], pixel[i, j][1], pixel[i, j][2])), fill_type='solid')
+            ws.cell(
+                row=j + 1, column=i + 1).fill = PatternFill(
+                    fgColor=Color('{0:02x}{1:02x}{2:02x}'.format(
+                        pixel[i, j][0], pixel[i, j][1], pixel[i, j][2])),
+                    fill_type='solid')
 
     for c in range(ws.max_column):
-        ws.column_dimensions[get_column_letter(c+1)].width = float(1.89)
+        ws.column_dimensions[get_column_letter(c + 1)].width = float(1.89)
     for r in range(ws.max_row):
-        ws.row_dimensions[r+1].height = float(14.4)
+        ws.row_dimensions[r + 1].height = float(14.4)
 
     wb.save(xlsx_file)
